a74006569d0ebd1fb42ae29ef86e15be7cec47aa
sneha <30837089+itmesneha@users.noreply.github.com>
2024-04-03 19:41:19 +0530

    Update 36. Valid Sudoku.py

--- 36. Valid Sudoku.py
+++ 36. Valid Sudoku.py
@@ -1,8 +1,19 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = collections.defaultdict(set) # r
+        #defaultdict will setup a dictionary with default value as empty set
+        rows = collections.defaultdict(set) # r 
         cols = collections.defaultdict(set) # c
         box = collections.defaultdict(set) # key = (r // 3, c // 3)
+
+        # hashset = defaultdict(set)
+        # print('hashset: ', hashset)
+
+        # for i in range(3):
+        #     hashset[i].add(i)
+        #     hashset[i].add(i)
+        #     hashset[i].add(i + 2)
+
+        print('hashset: ', hashset)
 
         for r in range(9):
             for c in range(9):
@@ -15,4 +26,7 @@ class Solution:
                 rows[r].add(board[r][c])
                 cols[c].add(board[r][c])
                 box[(r // 3 , c // 3)].add(board[r][c])
+                print('rows: ', rows)
+                print('cols: ', cols)
+                print('boxes: ', box)
         return True

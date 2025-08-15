3b77277fa51c32b6cb7072e8297d81b81e816206
sneha <30837089+itmesneha@users.noreply.github.com>
2025-08-15 12:12:28 +0530

    Time: 48 ms (72.57%) | Memory: 48.2 MB (29.56%) - LeetSync

--- 2529-range-product-queries-of-powers/range-product-queries-of-powers.py
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.py
@@ -5,11 +5,17 @@ class Solution:
         for i in range(32):
             if n & (1 << i) != 0:
                 powers.append(2**i)
-        # print(powers)
+        pre = []
+        temp = 1
+        for i in range(len(powers)):
+            temp *= powers[i]
+            pre.append(temp)
+        # print(pre)
         ans = []
         for [start, end] in queries:
-            res = 1
-            for i in range(start, end + 1):
-                res *= powers[i] % mod
-            ans.append(res % mod)
+            if start == 0:
+                den = 1
+            else:
+                den = pre[start - 1]
+            ans.append((pre[end] // den) % mod)
         return ans

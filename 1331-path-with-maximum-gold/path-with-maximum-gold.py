6a2435c394b7cccad7f1efa503c3829efc9f8841
sneha <30837089+itmesneha@users.noreply.github.com>
2025-08-17 21:55:06 +0530

    Time: 2536 ms (67.15%) | Memory: 17.2 MB (99.89%) - LeetSync

---
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.py
@@ -0,0 +1,20 @@
+class Solution:
+    def getMaximumGold(self, grid: List[List[int]]) -> int:
+        def fn(i, j, grid):
+            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] == 0 or grid[i][j] == -1:
+                return 0
+            gold = grid[i][j]
+            grid[i][j] = -1
+            max_gold = max(fn(i + 1, j, grid), fn(i-1, j, grid), fn(i, j+1, grid), fn(i, j - 1, grid))
+            grid[i][j] = gold
+            return gold + max_gold
+
+        res = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] != 0:
+                    res = max(res, fn(i, j, grid))
+
+        return res
+
+        

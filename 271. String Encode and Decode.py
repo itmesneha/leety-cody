0eab785d044df56d9db7aa22b468353ff4727f06
sneha <30837089+itmesneha@users.noreply.github.com>
2025-08-18 12:52:48 +0530

    Update 271. String Encode and Decode.py

--- 271. String Encode and Decode.py
+++ 271. String Encode and Decode.py
@@ -1,25 +1,23 @@
 class Solution:
 
-    # ['neet', 'code']
-    # '4#neet4#code'
-    
     def encode(self, strs: List[str]) -> str:
         res = ''
         for word in strs:
-            res = res + str(len(word)) + '#' + word
-        print('res: ', res)
+            res = res + str(len(word)) + '#'
+            res = res + word
+        print(res)
         return res
 
     def decode(self, s: str) -> List[str]:
         res = []
         i = 0
-
         while i < len(s):
+            count = ''
             j = i
             while s[j] != '#':
-                j = j + 1
-            length = int(s[i:j])
-            res.append(s[j + 1 : j + 1 + length])
-            i = j + 1 + length
+                j += 1
+            count = int(s[i : j])
+            res.append(s[j + 1:j + 1 + count])
+            i = j + 1 + count
         return res
 

87e6a3bfa00380b768cc83cb38cf0afbe598d8a0
sneha <30837089+itmesneha@users.noreply.github.com>
2025-08-24 19:31:19 +0530

    Time: 37 ms (20.62%) | Memory: 18.4 MB (34.55%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -5,20 +5,15 @@ class Solution:
 
         def feasible(row, col):
             for i in range(row):
-                for j in range(n):
-                    if board[i][j] == 'Q': 
-                        if j == col or abs(row-i) == abs(col-j):
-                            return False
+                # same column
+                if board[i][col] == 'Q':
+                    return False
+                # diagonal
+                if col - (row - i) >= 0 and board[i][col - (row - i)] == 'Q':
+                    return False
+                if col + (row - i) < n and board[i][col + (row - i)] == 'Q':
+                    return False
             return True
-            
-            # no need to check down as we are coming from up only
-            # for i in range(x, n):
-            #     for j in range(y, n):
-            #         if board[i][j] == 'Q': 
-            #             return False
-            #         if abs(x-i) == abs(y-j) and board[i][j] == 'Q':
-            #             return False
-
 
         def fn(row):
             if row == n:
@@ -33,20 +28,4 @@ class Solution:
         fn(0)
         return res
 
-        # count = 0
-        # res = []
-        # board = [['.' for _ in range(n)] for _ in range(n)]
-        # def fn(x, y, board, count):
-        #     if count == n:
-        #         for row in board:
-        #             res.append(''.join(row[:]))
-        #         return
-        #     for i in range(n):
-        #         for j in range(n):
-        #             if i != x and j != y and abs(i-x) != abs(j-y):
-        #                 board[i][j] = 'Q'
-        #                 fn(i, j, board, count + 1)
-        #                 board[i][j] = '.'
-
-        # fn(0, 0, board, 0)
-        # return res
+       

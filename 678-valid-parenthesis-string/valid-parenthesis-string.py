c67e88ed0aaa2de75985f0cf880e20b7de2cbd15
sneha <30837089+itmesneha@users.noreply.github.com>
2025-08-24 15:41:41 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (40.19%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -1,22 +1,48 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
         n = len(s)
-        self.memo = [[-1 for _ in range(n + 1)] for _ in range(n + 1)]
-        def fn(idx, open):
-            if open < 0:
-                return False
-            if idx >= len(s):
-                return open == 0
-            if self.memo[idx][open] != -1:
-                return self.memo[idx][open]
-            if s[idx] == '(':
-                self.memo[idx][open] = fn(idx + 1, open + 1)
-                return self.memo[idx][open]
-            elif s[idx] == ')':
-                self.memo[idx][open] = fn(idx + 1, open - 1)
-                return self.memo[idx][open]
+        stacky = []
+        asterisks = []
+        for i in range(n):
+            if s[i] == '(':
+                stacky.append(i)
+            elif s[i] == ')':
+                if stacky:
+                    stacky.pop()
+                elif asterisks:
+                    asterisks.pop()
+                else:
+                    return False
             else:
-                self.memo[idx][open] = fn(idx + 1, open + 1) or fn(idx + 1, open - 1) or fn(idx + 1, open)
-                return self.memo[idx][open]
+                asterisks.append(i)
+
+        while stacky and asterisks:
+            if stacky[-1] < asterisks[-1]:
+                stacky.pop()
+                asterisks.pop()
+            else:
+                return False
+
+        return not stacky
+
+        # n = len(s)
+        # memo = [[None for _ in range(n + 1)] for _ in range(n + 1)]
+        # def fn(idx, open):
+        #     if open < 0:
+        #         return False
+        #     if idx == n:
+        #         return open == 0
+        #     if memo[idx][open] is not None:
+        #         return memo[idx][open]
+
+        #     if s[idx] == '(':
+        #         ans = fn(idx + 1, open + 1)
+        #     elif s[idx] == ')':
+        #         ans = fn(idx + 1, open - 1)
+        #     else:
+        #         ans = fn(idx + 1, open + 1) or fn(idx + 1, open - 1) or fn(idx + 1, open)
+                
+        #     memo[idx][open] = ans
+        #     return ans
         
-        return fn(0, 0)
+        # return fn(0, 0)

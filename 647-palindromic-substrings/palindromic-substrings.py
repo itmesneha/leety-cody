c655164deeb64eb23db020a84f4cbbeae10912ae
sneha <30837089+itmesneha@users.noreply.github.com>
2025-08-26 00:43:27 +0530

    Time: 108 ms (88.00%) | Memory: 17.7 MB (87.84%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -2,29 +2,27 @@ class Solution:
     def countSubstrings(self, s: str) -> int:
         count = 0
         n = len(s)
-        memo = [-1 for _ in range(n+1)]
-        memo_palindrome = [[None for _ in range(n+1)] for _ in range(n+1)]
-        def isPalindrome(i, j):
-            if i > j:
-                return True
-            if memo_palindrome[i][j] != None:
-                return memo_palindrome[i][j]
-            if s[i] != s[j]:
-                return False
-            memo_palindrome[i][j] = isPalindrome(i+1, j-1)
-            return memo_palindrome[i][j]
+        # memo = [-1 for _ in range(n+1)]
+        # memo_palindrome = [[None for _ in range(n+1)] for _ in range(n+1)]
+        # def isPalindrome(i, j):
+        #     if i > j:
+        #         return True
+        #     if memo_palindrome[i][j] != None:
+        #         return memo_palindrome[i][j]
+        #     if s[i] != s[j]:
+        #         return False
+        #     memo_palindrome[i][j] = isPalindrome(i+1, j-1)
+        #     return memo_palindrome[i][j]
 
-        def fn(idx):
+        def expand(left, right):
             nonlocal count
-            if idx == n:
-                return
-            if memo[idx] != -1:
-                return memo[idx]
-            for j in range(idx, n):
-                if isPalindrome(idx, j):
-                    count += 1
-            memo[idx] = count
-            fn(idx + 1)
+            while left >= 0 and right < n and s[left] == s[right]:
+                count += 1
+                left -= 1
+                right += 1
 
-        fn(0)
+        for i in range(n):
+            expand(i, i)
+            expand(i, i + 1)
+
         return count

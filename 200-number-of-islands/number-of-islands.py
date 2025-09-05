03cbb991bb5e4c4bbcc7e9313a879301747342f2
sneha <30837089+itmesneha@users.noreply.github.com>
2025-09-05 17:13:26 +0530

    Time: 224 ms (96.03%) | Memory: 20.4 MB (43.22%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,32 +1,33 @@
+from collections import deque
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m = len(grid)
         n = len(grid[0])
         count = 0
-        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
         def bfs(i, j):
-            queue = []
-            queue.append([i, j])
-            grid[i][j] = '$'
+            queue = deque()
+            queue.append((i, j))
+            grid[i][j] = '$'  # mark visited
             while queue:
-                x, y = queue.pop(0)
-                for direction in directions:
-                    x_, y_ = x + direction[0], y + direction[1]
+                x, y = queue.popleft()
+                for dx, dy in directions:
+                    x_, y_ = x + dx, y + dy
                     if x_ < 0 or x_ >= m or y_ < 0 or y_ >= n or grid[x_][y_] != '1':
                         continue
-                    else:
-                        queue.append([x_, y_])
-                        grid[x_][y_] = '$'
+                    grid[x_][y_] = '$'
+                    queue.append((x_, y_))
 
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == '1':
                     bfs(i, j)
-                    # dfs(i, j)
                     count += 1
 
         return count
+
 
   # def dfs(i, j):
         #     if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] != '1':

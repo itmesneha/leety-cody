60b4fe626703380911083d2fba9f9eb5d3d6b17c
sneha <30837089+itmesneha@users.noreply.github.com>
2025-09-05 17:09:58 +0530

    Time: 254 ms (46.35%) | Memory: 20.3 MB (58.15%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -3,23 +3,40 @@ class Solution:
         m = len(grid)
         n = len(grid[0])
         count = 0
-        def dfs(i, j):
-            if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] != '1':
-                return # stop
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
 
+        def bfs(i, j):
+            queue = []
+            queue.append([i, j])
             grid[i][j] = '$'
-
-            dfs(i+1, j)
-            dfs(i-1, j)
-            dfs(i, j+1)
-            dfs(i, j-1)
+            while queue:
+                x, y = queue.pop(0)
+                for direction in directions:
+                    x_, y_ = x + direction[0], y + direction[1]
+                    if x_ < 0 or x_ >= m or y_ < 0 or y_ >= n or grid[x_][y_] != '1':
+                        continue
+                    else:
+                        queue.append([x_, y_])
+                        grid[x_][y_] = '$'
 
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == '1':
-                    dfs(i, j)
+                    bfs(i, j)
+                    # dfs(i, j)
                     count += 1
 
         return count
+
+  # def dfs(i, j):
+        #     if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] != '1':
+        #         return # stop
+
+        #     grid[i][j] = '$'
+
+        #     dfs(i+1, j)
+        #     dfs(i-1, j)
+        #     dfs(i, j+1)
+        #     dfs(i, j-1)
 
         

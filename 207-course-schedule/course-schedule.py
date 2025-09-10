17c0fe7a3adf31fc49bf5376410ebb4db4f731c0
sneha <30837089+itmesneha@users.noreply.github.com>
2025-09-10 12:27:21 +0530

    Time: 31 ms (5.79%) | Memory: 18.9 MB (93.19%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,26 @@
+from collections import deque
+class Solution:
+    def canFinish(self, n: int, pre: List[List[int]]) -> bool:
+        adj = [[] for _ in range(n)]
+        indegree = [0] * n
+        for v, u in pre:
+            adj[u].append(v)
+            indegree[v] += 1
+
+        q = deque()
+        for i in range(n):
+            if indegree[i] == 0:
+                q.append(i)
+        print('q: ', q)
+        nodecount = 0
+        while q:
+            cur = q.popleft()
+            print('q: ', q)
+            for child in adj[cur]:
+                indegree[child] -= 1
+                if indegree[child] == 0:
+                    q.append(child)
+            nodecount += 1
+            print('nodecount: ', nodecount)
+
+        return nodecount == n # no cycle therefore can take course , else not.

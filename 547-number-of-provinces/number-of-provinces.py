f1de227a8f2636987f63218bf5d3071043f36e7c
sneha <30837089+itmesneha@users.noreply.github.com>
2025-09-10 11:57:06 +0530

    Time: 7 ms (55.05%) | Memory: 19.6 MB (5.65%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -4,22 +4,27 @@ class Solution:
         visited = [False] * n
         count = 0
 
-        def bfs(city):
-            q = deque()
-            visited[city] = True
-            q.append(city)
-            while q:
-                cur = q.popleft()
-                for j in range(n):
-                    if isConnected[cur][j] and cur != j and not visited[j]:
-                        child = j
-                        visited[child] = True
-                        q.append(child)
+        # def bfs(city):
+        #     q = deque()
+        #     visited[city] = True
+        #     q.append(city)
+        #     while q:
+        #         cur = q.popleft()
+        #         for j in range(n):
+        #             if isConnected[cur][j] and cur != j and not visited[j]:
+        #                 child = j
+        #                 visited[child] = True
+        #                 q.append(child)
 
-
+        def dfs(cur):
+            visited[cur] = True
+            for j in range(n):
+                if isConnected[cur][j] and cur != j and not visited[j]:
+                    dfs(j)
 
         for city in range(n):
             if not visited[city]:
-                bfs(city)
+                # bfs(city)
+                dfs(city)
                 count += 1
         return count

a7d009507b96caeb6fe7458eefaa1d60ea2de811
sneha <30837089+itmesneha@users.noreply.github.com>
2025-09-12 13:06:04 +0530

    Time: 395 ms (6.47%) | Memory: 29.6 MB (6.07%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.py
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.py
@@ -1,34 +1,23 @@
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
         n = len(nums)
-        prefix_sum = [0] * n
+        l = 0
+        r = 0
+        pre = [0] * (n+1)
         cur = 0
         nums.sort()
-        # calculating prefix sum
         for i in range(n):
-            prefix_sum[i] = cur + nums[i]
-            cur = prefix_sum[i]
+            pre[i+1] = pre[i] + nums[i]
 
-        def binarysearch(target_index):
-            l = 0
-            r = target_index
-            best_mid = (l+r) // 2
-            while l <= r:
-                mid = (l+r) // 2
-                count = target_index - mid + 1
-                windowsum = count * nums[target_index]
-                ogsum = prefix_sum[target_index] - prefix_sum[mid] + nums[mid]
-                ops = windowsum - ogsum
-                if ops > k:
-                    l = mid + 1
-                else:
-                    best_mid = mid
-                    r = mid - 1
-            return target_index - best_mid + 1
+        freq = 1
+        while r < n:
+            windowsum = (r - l + 1) * nums[r]
+            ogsum = pre[r+1] - pre[l] 
 
-        ans = 1
-        for target_index in range(n):
-            freq = binarysearch(target_index)
-            ans = max(ans, freq)
+            if windowsum - ogsum <= k:
+                freq = max(freq, r - l + 1)
+                r += 1
+            else:
+                l += 1
 
-        return ans
+        return freq

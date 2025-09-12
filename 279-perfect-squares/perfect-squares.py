9c994b1b45c8e88413d420cdd314efc566b2ba81
sneha <30837089+itmesneha@users.noreply.github.com>
2025-09-13 00:14:13 +0530

    Time: 5059 ms (5.01%) | Memory: 21.4 MB (17.47%) - LeetSync

--- 279-perfect-squares/perfect-squares.py
+++ 279-perfect-squares/perfect-squares.py
@@ -7,7 +7,7 @@ class Solution:
             if memo[n] != -1:
                 return memo[n]
             i = 1
-            res = float(inf)
+            res = float('inf')
             while i <= sqrt(n):
                 res = min(res, 1+fn(n-(i**2)))
                 i += 1

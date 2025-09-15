c0d9c3163a92cb83c380e0aaa187b0cd042ea06c
sneha <30837089+itmesneha@users.noreply.github.com>
2025-09-15 11:45:34 +0530

    Time: 7 ms (69.29%) | Memory: 50.5 MB (8.94%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
@@ -1,3 +1,4 @@
+from collections import deque
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -8,11 +9,22 @@ class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        def fn(cur):
-            if not cur:
-                return float('inf') # invalid path
+        q = deque()
+        q.append((root,1))
+        while q:
+            cur, depth = q.popleft()
             if not cur.left and not cur.right:
-                return 1
-            return 1 + min(fn(cur.left), fn(cur.right))
+                return depth
+            if cur.left:
+                q.append((cur.left, depth + 1))
+            if cur.right:
+                q.append((cur.right, depth + 1))
+        
+        # def fn(cur):
+        #     if not cur:
+        #         return float('inf') # invalid path
+        #     if not cur.left and not cur.right:
+        #         return 1
+        #     return 1 + min(fn(cur.left), fn(cur.right))
 
-        return fn(root)
+        # return fn(root)

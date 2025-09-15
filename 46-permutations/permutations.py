cbc065079e5669549afbea57125db07a44dbafb6
sneha <30837089+itmesneha@users.noreply.github.com>
2025-09-15 11:16:03 +0530

    Time: 3 ms (31.10%) | Memory: 18.1 MB (16.61%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -2,18 +2,28 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
         res = []
-        def fn(cur, used):
-            if len(cur) == n:
-                res.append(cur[:])
-                return
-            for i in range(n):
-                if i in used: # for duplicate numbers also this will work
-                    continue
-                used.add(i)
-                cur.append(nums[i])
-                fn(cur, used)
-                used.remove(i)
-                cur.pop()
 
-        fn([], set())
+        def fn(idx):
+            if idx == n:
+                res.append(nums[:])
+            for i in range(idx, n):
+                nums[idx], nums[i] = nums[i], nums[idx]
+                fn(idx + 1)
+                nums[idx], nums[i] = nums[i], nums[idx]
+
+        fn(0)
+        # def fn(cur, used):
+        #     if len(cur) == n:
+        #         res.append(cur[:])
+        #         return
+        #     for i in range(n):
+        #         if i in used: # for duplicate numbers also this will work
+        #             continue
+        #         used.add(i)
+        #         cur.append(nums[i])
+        #         fn(cur, used)
+        #         used.remove(i)
+        #         cur.pop()
+
+        # fn([], set())
         return res

a8b4a3450c65cca18eb82b6ec8ac8b3fc126ff4a
sneha <30837089+itmesneha@users.noreply.github.com>
2025-09-17 17:40:46 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (17.28%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -1,3 +1,4 @@
+from collections import deque
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,23 +7,39 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        res = defaultdict(list)
-        if not root:
-            return []
-
-        def fn(node, level):
-            if not node:
-                return
-            res[level].append(node)
-            level = level + 1
-            fn(node.left, level)
-            fn(node.right, level )
-        fn(root , 1)
         ans = []
-        # print('res: ', res)
-        for level in res:
-            ans.append(res[level][-1].val)
+        if not root:
+            return ans
+
+        q = deque()
+        q.append(root)
+        while q:
+            rightSide = None
+            for _ in range(len(q)):
+                cur = q.popleft()
+                if cur:
+                    rightSide = cur
+                    q.append(cur.left)
+                    q.append(cur.right)
+            if rightSide:
+                ans.append(rightSide.val)
 
         return ans
 
-        # return res
+        # res = defaultdict(list)
+        # if not root:
+        #     return []
+
+        # def fn(node, level):
+        #     if not node:
+        #         return
+        #     res[level].append(node)
+        #     level = level + 1
+        #     fn(node.left, level)
+        #     fn(node.right, level )
+        # fn(root , 1)
+        # ans = []
+        # for level in res:
+        #     ans.append(res[level][-1].val)
+
+        # return ans

7363085db8bb79a4f025c13c5346336422936e18
sneha <30837089+itmesneha@users.noreply.github.com>
2025-09-19 23:44:47 +0530

    Time: 80 ms (68.88%) | Memory: 29.9 MB (20.61%) - LeetSync

--- 878-shifting-letters/shifting-letters.py
+++ 878-shifting-letters/shifting-letters.py
@@ -1,17 +1,29 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[int]) -> str:
         n = len(shifts)
-        diff = [0] * n
-        for i in range(n):
-            diff[0] += shifts[i]
-            if i + 1 < n:
-                diff[i + 1] -= shifts[i]
-
-        shift = 0
+        pre = 0
         res = []
-        for i in range(n):
-            shift += diff[i]
-            total_shift = (ord(s[i]) - ord('a') + shift) % 26
+        for i in range(n-1, -1, -1):
+            pre += shifts[i]
+            total_shift = (ord(s[i]) - ord('a') + pre) % 26 # [0-25]
             res.append(chr(ord('a') + total_shift))
 
-        return ''.join(res)
+        return ''.join(res[::-1])
+
+        # difference array technique
+
+        
+        # diff = [0] * n
+        # for i in range(n):
+        #     diff[0] += shifts[i]
+        #     if i + 1 < n:
+        #         diff[i + 1] -= shifts[i]
+
+        # shift = 0
+        # res = []
+        # for i in range(n):
+        #     shift += diff[i]
+        #     total_shift = (ord(s[i]) - ord('a') + shift) % 26 # [0-25]
+        #     res.append(chr(ord('a') + total_shift))
+
+        

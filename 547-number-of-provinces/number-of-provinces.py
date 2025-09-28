0a96241cfbfc8edea1d35b2ff1a7aabb214bb31a
sneha <30837089+itmesneha@users.noreply.github.com>
2025-09-28 20:17:26 +0530

    Time: 9 ms (30.78%) | Memory: 19.2 MB (37.70%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -17,13 +17,16 @@ class Solution:
         def explore(cur):
             q = deque()
             visited[cur] = True
-            q.append(cur)
+            q.append([cur, None])
             while q:
-                city = q.popleft()
+                city, parent = q.popleft()
                 for child in adj[city]:
+                    if child == parent:
+                        continue
+                    
                     if not visited[child]:
                         visited[child] = True
-                        q.append(child)
+                        q.append([child, cur])
 
         count = 0
         for city in range(n):
@@ -33,25 +36,4 @@ class Solution:
 
         return count
 
-        # directions = [[0,1],[0,-1],[1,0],[-1,0]]
-
-        # def explore(i, j):
-        #     q = deque()
-        #     isConnected[i][j] = -1
-        #     q.append([i, j])
-        #     while q:
-        #         x, y = q.popleft()
-        #         for d_x, d_y in directions:
-        #             n_x, n_y = x + d_x, y + d_y
-        #             if n_x >= 0 and n_x < m and n_y >= 0 and n_y < n and isConnected[n_x][n_y] == 1:
-        #                 isConnected[n_x][n_y] = -1    
-        #                 q.append([n_x, n_y])
-
-
-        # for i in range(m):
-        #     for j in range(n):
-        #         if isConnected[i][j] == 1:
-        #             explore(i, j)
-        #             count += 1
-
-        # return count
+       

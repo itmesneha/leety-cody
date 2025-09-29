f7852860415378e708f70f6adae8db96ede55949
sneha <30837089+itmesneha@users.noreply.github.com>
2025-09-30 01:02:49 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (54.90%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.py
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -1,3 +1,4 @@
+from collections import deque
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
         n = len(graph)
@@ -14,8 +15,25 @@ class Solution:
 
             return True
 
+        def bfs(cur, cur_color):
+            color[cur] = cur_color
+            q = deque()
+            q.append(cur)
+            while q:
+                node = q.popleft()
+                for child in graph[node]:
+                    if color[child] == color[node]:
+                        return False
+                    if color[child] == -1:
+                        color[child] = 1 - color[node]
+                        q.append(child)
+
+            return True
+
         for i in range(n):
             if color[i] == -1:
-                if not dfs(i, 0):
+            # this does not matter whether i start from 0 or 1 
+            # because this is for disconnected parts of the graph
+                if not bfs(i, 0): 
                     return False
         return True

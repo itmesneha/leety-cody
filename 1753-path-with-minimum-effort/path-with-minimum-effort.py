60cdace5896bff8d9516be5002c815503874f5ae
sneha <30837089+itmesneha@users.noreply.github.com>
2025-10-09 00:53:04 +0530

    Time: 377 ms (42.11%) | Memory: 19.4 MB (60.45%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.py
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.py
@@ -6,14 +6,12 @@ class Solution:
         result = [[float('inf')] * n for _ in range(m)]
         visited = [[False] * n for _ in range(m)]
         minheap = []
-        i = 0
-        j = 0
-        h.heappush(minheap, (0, i, j, heights[i][j])) # cost, x, y , old heights value
-        heights[i][j] = -1 # mark as visited
-        result[i][j] = 0 # cost to source = 0
+        h.heappush(minheap, (0, 0, 0)) # cost, x, y , old heights value
+        visited[0][0] = True # mark as visited
+        result[0][0] = 0 # cost to source = 0
         directions = [[0,1],[0,-1],[1,0],[-1,0]] # up, down, left, right
         while minheap:
-            d, x, y, val = h.heappop(minheap) # pop top 
+            d, x, y = h.heappop(minheap) # pop top 
             visited[x][y] = True
             for add_x, add_y in directions: # for each direction
                 # find new coordinates
@@ -22,11 +20,11 @@ class Solution:
                 # check within boundaries new coordinates
                 if new_x >= 0 and new_x < m and new_y >= 0 and new_y < n: 
                     # find abs difference
-                    new_cost = max(d, abs(heights[new_x][new_y] - val))
+                    new_cost = max(d, abs(heights[new_x][new_y] - heights[x][y]))
                     if new_cost < result[new_x][new_y]: # if cost is less
                         result[new_x][new_y] = new_cost # update result
                         # add to heap
-                        h.heappush(minheap, (new_cost, new_x, new_y, heights[new_x][new_y])) 
+                        h.heappush(minheap, (new_cost, new_x, new_y)) 
 
 
         if result[m-1][n-1] != float('inf'):

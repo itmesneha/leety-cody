63e435710a0c91e376ebf2adb68aea868d9e1bc3
sneha <30837089+itmesneha@users.noreply.github.com>
2025-11-25 03:16:46 +0530

    Time: 11 ms (83.16%) | Memory: 20.8 MB (61.58%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,11 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        di = {}
+        # in map the keys will be maps but maps, lists are unhashable
+        # [e:1, a:1, t:1] : [eat,tea,ate]
+        main_map = defaultdict(list)
         for word in strs:
-            temp = sorted(word)
-            sorted_word = ''.join(temp)
-            # print(sorted_word)
-            lst = di.get(sorted_word, [])
-            lst.append(word)
-            di[sorted_word] = lst
-        return(list(di.values()))
+            # print(''.join(sorted(word)))
+            main_map[''.join(sorted(word))].append(word)
+
+        # print(values(main_map))
+        return list(main_map.values())

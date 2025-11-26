857306b2d839b8b99d0dc3f39378780cb6c0495f
sneha <30837089+itmesneha@users.noreply.github.com>
2025-11-27 01:14:10 +0530

    Time: 24 ms (55.12%) | Memory: 25.3 MB (36.57%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -7,18 +7,16 @@ class Solution:
         '''
         n = len(nums)
         prefix = [1] * n
-        suffix = [1] * n
+        suffix = 1
         prefix[0] = nums[0]
-        suffix[n-1] = nums[n-1]
         for i in range(1,n):
             prefix[i] = prefix[i-1] * nums[i]
-            suffix[n-i-1] = suffix[n-i] * nums[n-i-1]
-            # print('prefix: ', prefix)
-            # print('suffix: ',suffix)
         res = [1] * n
-        res[0] = suffix[1]
         res[n-1] = prefix[n-2]
-        for i in range(1,n-1):
-            res[i] = prefix[i-1] * suffix[i+1]
+        suffix = nums[n-1]
+        for i in range(n-2, 0, -1):
+            res[i] = prefix[i-1] * suffix
+            suffix *= nums[i]
             # print(res)
+        res[0] = suffix
         return res

19c193f24f7660243584e62101b7d9dff0896975
sneha <30837089+itmesneha@users.noreply.github.com>
2025-11-28 03:43:41 +0530

    Time: 4 ms (13.04%) | Memory: 17.8 MB (58.25%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        d = {}
+        d[')'] = '('
+        d[']'] = '['
+        d['}'] = '{'
+
+        stack = []
+        for bracket in s:
+            if bracket in d.values():
+                stack.append(bracket)
+            else:
+                if stack:
+                    temp = stack.pop()
+                    if d[bracket] != temp:
+                        return False
+                else:
+                    return False
+        if stack:
+            return False
+        return True

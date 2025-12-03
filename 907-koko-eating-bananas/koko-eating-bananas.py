2ae2df39f4d8fcced1040e25b2faf4d5031464df
sneha <30837089+itmesneha@users.noreply.github.com>
2025-12-03 23:38:24 +0530

    Time: 277 ms (5.14%) | Memory: 19.2 MB (38.49%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        '''
+        binary search on answer ie k
+        max = ceil(total bananas / hours)
+        min = 1
+        '''
+        min_k = 1
+        max_k = max(piles)
+        n = len(piles)
+        while min_k <= max_k:
+            k = (min_k + max_k) // 2
+            print('k: ', k)
+            time = 0
+            for i in range(n):
+                if piles[i] - k <= 0:
+                    time += 1
+                elif piles[i] % k == 0:
+                    time += piles[i] // k
+                else:
+                    time += (piles[i] // k) + 1
+            print('time: ', time)
+            if time > h:
+                min_k = k + 1
+            elif time <= h:
+                max_k = k - 1
+            
+        return min_k

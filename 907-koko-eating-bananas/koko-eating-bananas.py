4e9da9c311a56055956f77cf1e6f05f04edc1455
sneha <30837089+itmesneha@users.noreply.github.com>
2025-12-03 23:40:39 +0530

    Time: 158 ms (53.85%) | Memory: 18.9 MB (97.24%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -10,14 +10,14 @@ class Solution:
         n = len(piles)
         while min_k <= max_k:
             k = (min_k + max_k) // 2
-            time = 0
-            for i in range(n):
-                if piles[i] - k <= 0:
-                    time += 1
-                elif piles[i] % k == 0:
-                    time += piles[i] // k
-                else:
-                    time += (piles[i] // k) + 1
+            time = sum(ceil(x/k) for x in piles)
+            # for i in range(n):
+            #     if piles[i] - k <= 0:
+            #         time += 1
+            #     elif piles[i] % k == 0:
+            #         time += piles[i] // k
+            #     else:
+            #         time += (piles[i] // k) + 1
             if time > h:
                 min_k = k + 1
             elif time <= h:

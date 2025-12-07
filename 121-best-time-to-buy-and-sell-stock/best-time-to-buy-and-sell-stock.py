227672575f1f9f606f5cf28e454dc2faf580dd23
sneha <30837089+itmesneha@users.noreply.github.com>
2025-12-08 03:42:59 +0530

    Time: 71 ms (70.32%) | Memory: 27.1 MB (8.59%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,21 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         '''
-        if i buy a stock on one day, need to find minimum value on right side of it.
+        greedy approach
         '''
         profit = 0
         n = len(prices)
-        max_from_right = []
-        cur_max = float('-inf')
-        for i in range(n-1, -1, -1):
-            if prices[i] > cur_max:
-                cur_max = prices[i]
+        left = 0
+        for right in range(1, n):
+            if prices[right] > prices[left]:
+                profit = max(profit, prices[right] - prices[left])
 
-            max_from_right.append(cur_max)
-
-        max_from_right = max_from_right[::-1]
-        # print('max_from_right: ', max_from_right)
-        for i in range(n):
-            profit = max(profit, max_from_right[i] - prices[i])
+            else:
+                left = right
 
         return profit

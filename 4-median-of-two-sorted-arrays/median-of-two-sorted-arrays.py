e6e80301dc9b5e876deb904d323796a4e23338d9
sneha <30837089+itmesneha@users.noreply.github.com>
2025-12-08 02:11:26 +0530

    Time: 7 ms (16.75%) | Memory: 17.9 MB (82.54%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -11,7 +11,6 @@ class Solution:
         n1 = len(nums1)
         n2 = len(nums2)
         total = n1 + n2
-        temp = []
         counter = 0
         if total % 2 == 0:
             index_second = total // 2
@@ -21,17 +20,13 @@ class Solution:
             index_second = total // 2
             index_first = total // 2
         
-        print('index_first: ', index_first)
-        print('index_second: ', index_second)
         while p1 < n1 and p2 < n2:
             if nums1[p1] <= nums2[p2]:
                 if counter == index_second:
                     second_element = nums1[p1]
                 if counter == index_first:
                     first_element = nums1[p1]
-                    print('first element found!')
                 p1 += 1
-                print('p1: ', p1)
 
             else:
                 if counter == index_second:
@@ -40,54 +35,33 @@ class Solution:
                     first_element = nums2[p2]
 
                 p2 += 1
-                print('p2: ', p2)
 
             counter += 1
 
         while p1 < n1:
             if counter == index_first:
                 first_element = nums1[p1]
-                print('first element found!')
+
             if counter == index_second:
                 second_element = nums1[p1]
-                print('second element found!')
                 break
             p1 += 1
-            print('p1: ', p1)
+
             counter += 1
            
         while p2 < n2:
             if counter == index_first:
                 first_element = nums2[p2]
-                print('first element found!')
+
             if counter == index_second:
                 second_element = nums2[p2]
-                print('second element found!')
                 break
             p2 += 1
-            print('p2: ', p2)
+
             counter += 1
 
         if index_first == index_second:
             return  first_element
         return (first_element + second_element) / 2
 
-        # # print('temp: ', temp)
-        # if p1 < n1:
-        #     for x in nums1[p1:]:
-        #         temp.append(x)
-           
-        # if p2 < n2:
-        #      for x in nums2[p2:]:
-        #         temp.append(x)
-
-        # # print('temp: ', temp)
-        # if (n1 + n2) % 2 == 0: # even
-        #     second = total // 2
-        #     first = second - 1
-        #     median = (temp[first] + temp[second]) / 2
-        
-        # else:
-        #     median = temp[total // 2]
-
-        # return median
+      

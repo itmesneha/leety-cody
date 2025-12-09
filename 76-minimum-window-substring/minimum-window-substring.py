a75cf2ee0f4458d83154f68b6992023cbe0af6b1
sneha <30837089+itmesneha@users.noreply.github.com>
2025-12-09 18:15:34 +0530

    Time: 123 ms (35.55%) | Memory: 18.5 MB (15.21%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -8,21 +8,47 @@ class Solution:
         l = 0
         ans = ''
         needed = len(t_map)
-        for r in range(len(s)):
-            if s[r] in t_map:
-                s_map[s[r]] += 1
-                if s_map[s[r]] == t_map[s[r]]:
+
+        for r, ch in enumerate(s):
+            if ch in t_map:
+                s_map[ch] += 1
+                if s_map[ch] == t_map[ch]:
                     needed -= 1
-            while needed == 0:
-                if r - l + 1 < min_len:
-                    min_len = r - l + 1
+
+            while needed == 0 and l <= r:
+                window = r - l + 1
+                if window < min_len:
+                    min_len = window
                     ans = s[l:r+1]
-                # contract
-                if s[l] in t_map:
-                    s_map[s[l]] -= 1
-                if s_map[s[l]] < t_map[s[l]]:
-                    needed += 1
+                left_char = s[l]
+                if left_char in t_map:
+                    s_map[left_char] -= 1
+                    if s_map[left_char] < t_map[left_char]:
+                        needed += 1
+
                 l += 1
 
         return ans
+
+
+
+
+
+        # for r in range(len(s)):
+        #     if s[r] in t_map:
+        #         s_map[s[r]] += 1
+        #         if s_map[s[r]] == t_map[s[r]]:
+        #             needed -= 1
+        #     while needed == 0:
+        #         if r - l + 1 < min_len:
+        #             min_len = r - l + 1
+        #             ans = s[l:r+1]
+        #         # contract
+        #         if s[l] in t_map:
+        #             s_map[s[l]] -= 1
+        #         if s_map[s[l]] < t_map[s[l]]:
+        #             needed += 1
+        #         l += 1
+
+        # return ans
 

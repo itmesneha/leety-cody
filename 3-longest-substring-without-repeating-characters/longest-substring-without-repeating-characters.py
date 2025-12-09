063138ffe62082689e7c7e5635d28f130e2c7f90
sneha <30837089+itmesneha@users.noreply.github.com>
2025-12-09 15:27:38 +0530

    Time: 27 ms (22.11%) | Memory: 18 MB (11.79%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,20 +1,19 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        map = defaultdict(int)
+        '''
+        sliding window
+        '''
+        counts = defaultdict(int)
+        l = 0
         n = len(s)
         ans = 0
-        start_index = 0
-        for end_index in range(n):
-            # if letter already in map
-            if s[end_index] in map and s[end_index] != -1:
-                del_index = map[s[end_index]]
-                while start_index <= del_index:
-                    map[s[start_index]] = -1
-                    start_index += 1
-
-            # if letter not in map
-            map[s[end_index]] = end_index
-
-            ans = max(ans, end_index - start_index + 1)
+        for r in range(n):
+            counts[s[r]] += 1
+            # contract the window
+            while counts[s[r]] > 1:
+                counts[s[l]] -= 1
+                l += 1
+            # now you should have valid window
+            ans = max(ans, r - l + 1)
 
         return ans

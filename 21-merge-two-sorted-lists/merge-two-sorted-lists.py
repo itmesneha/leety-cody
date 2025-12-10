8923f0930e06c13d6c65caf5b12f63c35e1d54d7
sneha <30837089+itmesneha@users.noreply.github.com>
2025-12-10 17:20:05 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (60.79%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -4,30 +4,44 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         '''
-        2 pointer
+        recursion
         '''
-        p1 = list1
-        p2 = list2
-        head = ListNode()
-        cur = head
-        while p1 and p2:
-            if p1.val <= p2.val:
-                cur.next = p1
-                p1 = p1.next
-            else:
-                cur.next = p2
-                p2 = p2.next
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
 
-            cur = cur.next
 
-        if p1:
-            cur.next = p1
+        # 2 pointer
+        # '''
+        # p1 = list1
+        # p2 = list2
+        # head = ListNode()
+        # cur = head
+        # while p1 and p2:
+        #     if p1.val <= p2.val:
+        #         cur.next = p1
+        #         p1 = p1.next
+        #     else:
+        #         cur.next = p2
+        #         p2 = p2.next
+
+        #     cur = cur.next
+
+        # if p1:
+        #     cur.next = p1
         
-        if p2:
-            cur.next = p2
+        # if p2:
+        #     cur.next = p2
 
-        return head.next
+        # return head.next
 
         

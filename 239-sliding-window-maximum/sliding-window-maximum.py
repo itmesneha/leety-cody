aff93967f3ca02274d93fda744d497e6745310c6
sneha <30837089+itmesneha@users.noreply.github.com>
2025-12-10 16:53:43 +0530

    Time: 211 ms (24.76%) | Memory: 31.5 MB (54.48%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -3,25 +3,52 @@ import heapq as h
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         '''
-        using heap
+        using deque
         '''
-        max_heap = []
-        ans = []
+        queue = deque()
         n = len(nums)
-        l = 0
-        r = k-1
-        for i in range(l, r+1):
-            h.heappush(max_heap, [-nums[i], i])
-        ans.append(-1 * max_heap[0][0])
-        l += 1
-        r += 1
+        l,r = 0,0
+        ans = []
         while r < n:
-            h.heappush(max_heap, [-nums[r], r])
-            # if max number no longer in window
-            while max_heap[0][1] < l : 
-                h.heappop(max_heap)
-            ans.append(-1 * max_heap[0][0])
-            l += 1
+            # print(f'queue: {queue}')x
+            while queue and nums[r] > nums[queue[0]]:
+                queue.popleft()
+
+            if queue and l > queue[0]:
+                queue.popleft()
+
+            while queue and nums[r] > nums[queue[-1]]:
+                queue.pop()
+
+            queue.append(r)
+            
+            if r >= k-1:
+                ans.append(nums[queue[0]])
+                l += 1
+            # print(f'ans: {ans}')
             r += 1
+            
         return ans
+
+
+        # using heap
+        # max_heap = []
+        # ans = []
+        # n = len(nums)
+        # l = 0
+        # r = k-1
+        # for i in range(l, r+1):
+        #     h.heappush(max_heap, [-nums[i], i])
+        # ans.append(-1 * max_heap[0][0])
+        # l += 1
+        # r += 1
+        # while r < n:
+        #     h.heappush(max_heap, [-nums[r], r])
+        #     # if max number no longer in window
+        #     while max_heap[0][1] < l : 
+        #         h.heappop(max_heap)
+        #     ans.append(-1 * max_heap[0][0])
+        #     l += 1
+        #     r += 1
+        # return ans
 

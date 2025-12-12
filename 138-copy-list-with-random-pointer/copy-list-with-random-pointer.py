4b2e35c6468b837594931849749f7a648dadde9b
sneha <30837089+itmesneha@users.noreply.github.com>
2025-12-12 16:49:26 +0530

    Time: 46 ms (22.08%) | Memory: 18.4 MB (84.83%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -9,29 +9,49 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        '''
+        maintain old:new mapping or intervleave nodes
+        '''
         if not head:
             return None
-            
+
         cur, prev = head, head
-        dummy = ListNode(0)
+        dummy = Node(0)
         prev_copy, cur_copy = dummy, dummy
         old_new = {}
         while cur:
-            cur_copy = ListNode(cur.val)
-            old_new[cur] = cur_copy # mapping old:new
-            prev_copy.next = cur_copy
-            prev_copy = cur_copy
-            prev = cur
+            cur_copy = Node(cur.val)
+            cur_copy.next = cur.next
+            cur.next = cur_copy
+            cur = cur.next.next
+
+        # random pointers
+        cur = head
+        while cur and cur.next:
+            copy = cur.next
+            if cur.random:
+                copy.random = cur.random.next
+            else:
+                copy.random = None
+            cur = copy.next
+
+        # extract new list
+        new_head = head.next
+        cur = new_head
+        while cur and cur.next:
+            cur.next = cur.next.next
             cur = cur.next
 
-        old = head # old list
-        while old:
-            new = old_new[old]
-            if old.random:
-                new.random = old_new[old.random]
-            else:
-                new.random = None
-            
-            old = old.next
+        return new_head
 
-        return old_new[head]
+        # old = head # old list
+        # while old:
+        #     new = old_new[old]
+        #     if old.random:
+        #         new.random = old_new[old.random]
+        #     else:
+        #         new.random = None
+            
+        #     old = old.next
+
+        # return old_new[head]

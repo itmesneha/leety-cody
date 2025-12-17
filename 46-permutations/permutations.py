b2921e052978caa441a6da347777d2e237b03e2c
sneha <30837089+itmesneha@users.noreply.github.com>
2025-12-17 17:14:34 +0530

    Time: 1 ms (46.96%) | Memory: 18 MB (58.85%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,29 +1,20 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
-        res = []
+        '''
+        Fix one position at a time by swapping each possible choice into it, recurse, then swap back.
+        '''
+        self.res = []
 
-        def fn(idx):
+        def fn(nums, idx):
+            n = len(nums)
             if idx == n:
-                res.append(nums[:])
+                self.res.append(nums[:])
+                return
+
             for i in range(idx, n):
-                nums[idx], nums[i] = nums[i], nums[idx]
-                fn(idx + 1)
+                nums[i], nums[idx] = nums[idx], nums[i]
+                fn(nums, idx + 1)
                 nums[idx], nums[i] = nums[i], nums[idx]
 
-        fn(0)
-        # def fn(cur, used):
-        #     if len(cur) == n:
-        #         res.append(cur[:])
-        #         return
-        #     for i in range(n):
-        #         if i in used: # for duplicate numbers also this will work
-        #             continue
-        #         used.add(i)
-        #         cur.append(nums[i])
-        #         fn(cur, used)
-        #         used.remove(i)
-        #         cur.pop()
-
-        # fn([], set())
-        return res
+        fn(nums, 0)
+        return self.res

f8dd9d1f091fa0c8d8f2af48e8ccf563d8611ca2
sneha <30837089+itmesneha@users.noreply.github.com>
2025-12-17 22:42:36 +0530

    Time: 3 ms (5.83%) | Memory: 18 MB (22.13%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,13 +1,18 @@
 class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
+    def letterCombinations(self, nums: str) -> List[str]:
         self.dic = {'2':['a','b','c'], '3':['d','e','f'],'4':['g','h','i'], '5':['j','k','l'], '6':['m','n','o'], '7':['p','q','r','s'], '8':['t','u','v'], '9':['w','x','y','z']}
         self.res = []
-        def fn(idx1, ans):
-            if idx1 == len(digits):
-                if len(ans) > 0:
-                    self.res.append(ans)
+        n = len(nums)
+        def fn(idx, op):
+            if idx == n:
+                self.res.append(''.join(op[:]))
                 return
-            for letter in self.dic[digits[idx1]]:
-                fn(idx1 + 1, ans + letter)
-        fn(0, '')
+
+            for val in self.dic[nums[idx]]:
+                op.append(val)
+                fn(idx + 1, op)
+                op.pop()
+
+
+        fn(0, [])
         return self.res

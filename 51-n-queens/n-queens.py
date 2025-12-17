d18e7e86ff27605c81a01eba8c4857a817184cb8
sneha <30837089+itmesneha@users.noreply.github.com>
2025-12-18 01:38:59 +0530

    Time: 7 ms (96.15%) | Memory: 18.2 MB (81.76%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -2,31 +2,31 @@ class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         res = []
         board = [['.' for _ in range(n)] for _ in range(n)]
-        columns = set()
+        cols = set()
         diagonals = set()
         antidiagonals = set()
 
         def fn(row):
             if row == n:
                 res.append([''.join(r) for r in board])
-                return
+                return 
+
             for col in range(n):
-                if col in columns or row + col in diagonals or row - col in antidiagonals:
+                if col in cols or row + col in diagonals or row - col in antidiagonals:
                     continue
-                # place & explore
+
                 board[row][col] = 'Q'
-                columns.add(col)
+                cols.add(col)
                 diagonals.add(row + col)
                 antidiagonals.add(row - col)
+
                 fn(row + 1)
 
-                # backtrack
                 board[row][col] = '.'
-                columns.remove(col)
+                cols.remove(col)
                 diagonals.remove(row + col)
                 antidiagonals.remove(row - col)
 
         fn(0)
         return res
 
-       

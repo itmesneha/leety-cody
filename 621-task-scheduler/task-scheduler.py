ef7100741e75c66af7c602597a53452eb0508fa6
sneha <30837089+itmesneha@users.noreply.github.com>
2025-12-18 22:33:41 +0530

    Time: 86 ms (64.38%) | Memory: 19.4 MB (7.85%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -1,34 +1,34 @@
-from collections import Counter
-
-class Solution:
-    def leastInterval(self, tasks, n):
-        '''
-        Max frequency creates gaps, others fill them, leftover gaps become idle.
-        '''
-        from collections import Counter
+import heapq as h
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        '''
-        try to complete the tasks that have largest frequency first
+        freq = [0] * 26
+        for task in tasks:
+            freq[ord(task) - ord('A')] += 1
 
-        '''
-        freq = Counter(tasks)
         print(freq)
-        maxfreq = max(freq.values())
-        empty_blocks = maxfreq-1
-        idle_spots = empty_blocks * n
-        count = 1
-        for task in freq:
-            if freq[task] == maxfreq and count == 1:
-                count -= 1
+
+        time = 0
+        maxheap = []
+
+        for element in freq:
+            if element > 0:
+                h.heappush(maxheap, -element)
+
+        while maxheap:
+            temp = []
+            for i in range(1, n+2):
+                if maxheap:
+                    f = -1 * h.heappop(maxheap)
+                    f -= 1
+                    temp.append(f)
+
+            for f in temp:
+                if f > 0:
+                    h.heappush(maxheap, -f)
+
+            if not maxheap: # empty
+                time += len(temp)
             else:
-                idle_spots -= min(freq[task], empty_blocks)
+                time += n+1
 
-        if idle_spots > 0:
-            return len(tasks) + idle_spots
-
-        else:
-            return len(tasks)
-
-
-        return max(len(tasks), cost)
+        return time

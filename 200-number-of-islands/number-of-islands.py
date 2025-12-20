328974de179735f0a44aed223476d226a41ac391
sneha <30837089+itmesneha@users.noreply.github.com>
2025-12-21 03:25:47 +0530

    Time: 288 ms (15.34%) | Memory: 19.7 MB (99.46%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,24 +1,24 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+
+        def explore(x, y):
+            dir = [[0,1], [0,-1], [1,0], [-1,0]]
+
+            if x < 0 or x == m or y < 0 or y == n or grid[x][y] =='0' or grid[x][y] == '-1':
+                return
+
+            for dx, dy in dir:
+                grid[x][y] = '-1' # mark as visited
+                explore(x + dx, y + dy)
+                # grid[x][y] = '1' 
+
         m = len(grid)
         n = len(grid[0])
         count = 0
-        directions = [[0,1], [0,-1], [1,0], [-1,0]]
-
-        def explore(i, j):
-            if i < 0 or i == m or j < 0 or j == n or grid[i][j] != '1':
-                return
-            grid[i][j] = -1
-            for di, dj in directions:
-                explore(i + di, j + dj)
-            
-
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == '1': # != -1
+                if grid[i][j] == '1':
                     explore(i, j)
                     count += 1
 
         return count
-
-    

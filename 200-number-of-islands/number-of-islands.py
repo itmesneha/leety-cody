34f2e87a4210c6dbd91996f69a74a204d394b542
sneha <30837089+itmesneha@users.noreply.github.com>
2025-12-21 03:33:05 +0530

    Time: 230 ms (88.89%) | Memory: 20 MB (94.41%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,17 +1,32 @@
+from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
 
-        def explore(x, y):
-            dir = [[0,1], [0,-1], [1,0], [-1,0]]
+        def explore(x,y): # bfs
+            q = deque()
+            grid[x][y] = '-1'
+            q.append((x,y))
+            while q:
+                curx, cury = q.popleft()
+                for dx, dy in dir:
+                    nx = curx + dx
+                    ny = cury + dy
+                    if nx < 0 or nx == m or ny < 0 or ny == n or grid[nx][ny] != '1':
+                        continue
+                    grid[nx][ny] = '-1'
+                    q.append((nx, ny))
 
-            if x < 0 or x == m or y < 0 or y == n or grid[x][y] =='0' or grid[x][y] == '-1':
-                return
+        # def explore(x, y):
 
-            for dx, dy in dir:
-                grid[x][y] = '-1' # mark as visited
-                explore(x + dx, y + dy)
-                # grid[x][y] = '1' 
+        #     if x < 0 or x == m or y < 0 or y == n or grid[x][y] =='0' or grid[x][y] == '-1':
+        #         return
 
+        #     for dx, dy in dir:
+        #         grid[x][y] = '-1' # mark as visited
+        #         explore(x + dx, y + dy)
+        #         # grid[x][y] = '1' # no need for this as we want to classify all reachable positions in 1 island 
+
+        dir = [[0,1], [0,-1], [1,0], [-1,0]]
         m = len(grid)
         n = len(grid[0])
         count = 0

f5eda2ec4e954ad2e2731465281e86fad1cdf52b
sneha <30837089+itmesneha@users.noreply.github.com>
2026-01-01 16:24:46 +0530

    Update 286. Walls and Gates.py

--- 286. Walls and Gates.py
+++ 286. Walls and Gates.py
@@ -1,21 +1,49 @@
 from collections import deque
 class Solution:
     def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        '''
+        shortest path in unweighted graph = BFS
+
+        Imagine all gates flooding the grid at the same time.
+        The water that reaches a cell first is from the nearest gate.
+        That’s multi-source BFS.
+
+        Add all treasure to q, then keep adding one at each level.
+        '''
+
+        q = deque()
+
         m = len(grid)
         n = len(grid[0])
         inf = (2**31) - 1
-        q = deque()
+        # push all treasure chests into deque first
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 0:
-                    q.append((i, j))
+                    q.append((i,j))
 
-        directions = [(0,1), (0,-1), (1,0), (-1, 0)]
+        directions = [[0,1], [0,-1], [1,0], [-1,0]]
 
         while q:
-            i, j = q.popleft()
-            for di, dj in directions:
-                ni, nj = i + di, j + dj
-                if ni >= 0 and ni < m and nj >= 0 and nj < n and grid[ni][nj] == inf:
-                    grid[ni][nj] = grid[i][j] + 1
-                    q.append((ni, nj))
+            x,y = q.popleft() # pop treasure chest
+            
+            # begin exploration
+            for dx, dy in directions:
+                nx, ny = x + dx, y + dy
+                
+                if nx < 0 or nx == m or ny < 0 or ny == n or grid[nx][ny] != inf:
+                    continue
+                
+                # if grid[nx][ny] == float('inf')
+                grid[nx][ny] = grid[x][y] + 1 
+                # as we expand outwards from each x,y in q 
+                # we can just add 1. initially the value of treasure points is 0, 
+                # the first ones we reach from it would be 0 + 1
+                q.append((nx,ny))
+
+
+
+
+
+
+

254ff0316e85561831017e72ee53012457ecac99
sneha <30837089+itmesneha@users.noreply.github.com>
2026-01-02 21:29:35 +0530

    Time: 7 ms (45.42%) | Memory: 18.3 MB (99.43%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,24 +1,45 @@
-from collections import defaultdict, deque
+from collections import deque
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        adj = defaultdict(list)
-        indegree = [0] * numCourses
-        for v,u in prerequisites:
-            adj[u].append(v)
-            indegree[v] += 1
+        '''
+        Kahn's Algorithm for topological sort
+        1. first calculate adjacency matrix (dictionary node --> connections) and indegrees
+        2. put nodes with indegree = 0 in deque
+        3. while q:
+            pop from q
+            append to result
+            add to count + 1
+            for each connection:
+                subtract 1 from indegree
+                if for that node indegree == 0:
+                    append to deque
+
+        4. if count == n then no cycle, else cycle (no topoligcal sort)
+        '''
+
+        adj = [[] for _ in range(numCourses)]
+        indegree = [0 for _ in range(numCourses)]
+
+        for course, pre in prerequisites:
+            adj[pre].append(course)
+            indegree[course] += 1
 
         q = deque()
         for i in range(numCourses):
             if indegree[i] == 0:
                 q.append(i)
-        
+
         count = 0
         while q:
-            cur = q.pop()
+            cur = q.popleft()
             count += 1
-            for child in adj[cur]:
-                indegree[child] -= 1
-                if indegree[child] == 0:
-                    q.append(child)
+            for neighbor in adj[cur]:
+                indegree[neighbor] -=1 
+                if indegree[neighbor] == 0:
+                    q.append(neighbor)
 
-        return count == numCourses
+        if count == numCourses:
+            return True
+
+        return False
+

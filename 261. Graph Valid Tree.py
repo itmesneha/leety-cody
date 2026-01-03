91a1becec0ef9eb1a012d529f322e54bfcdf7b27
sneha <30837089+itmesneha@users.noreply.github.com>
2026-01-03 19:50:36 +0530

    Create 261. Graph Valid Tree.py

---
+++ 261. Graph Valid Tree.py
@@ -0,0 +1,49 @@
+from collections import deque
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        '''
+        Cannot use Kahn's algo here:
+        Why indegree logic breaks for undirected graphs:
+            In a tree:
+                Every node except leaves has indegree ≥ 1
+                Leaves have indegree = 1 (not 0)
+
+        An undirected graph is a tree iff:
+            - Connected
+            - No cycles
+            - edges = n - 1
+
+        You only need two checks:
+            - len(edges) == n - 1
+            - graph is connected
+        '''
+        if len(edges) != n-1:
+            return False 
+            
+        adj = [[] for _ in range(n)]
+
+        for u,v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+
+        q = deque()
+        q.append(0)
+        visited = set()
+        visited.add(0)
+
+        while q:
+            cur = q.popleft()
+            for neighbor in adj[cur]:
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    q.append(neighbor)
+
+
+        if len(visited) == n:
+            return True
+
+        return False
+
+
+
+

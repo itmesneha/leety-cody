96a8feb2c6efacc5dd6afbd0223c9c4416f913a9
sneha <30837089+itmesneha@users.noreply.github.com>
2026-01-08 15:16:15 +0530

    Time: 116 ms (81.38%) | Memory: 19.3 MB (23.50%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -1,19 +1,11 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        count = 0
-        n = len(s)
-        # memo = [-1 for _ in range(n+1)]
-        # memo_palindrome = [[None for _ in range(n+1)] for _ in range(n+1)]
-        # def isPalindrome(i, j):
-        #     if i > j:
-        #         return True
-        #     if memo_palindrome[i][j] != None:
-        #         return memo_palindrome[i][j]
-        #     if s[i] != s[j]:
-        #         return False
-        #     memo_palindrome[i][j] = isPalindrome(i+1, j-1)
-        #     return memo_palindrome[i][j]
+        '''
+        expand way?
+        '''
 
+        n = len(s)
+        count = 0
         def expand(left, right):
             nonlocal count
             while left >= 0 and right < n and s[left] == s[right]:
@@ -22,7 +14,9 @@ class Solution:
                 right += 1
 
         for i in range(n):
-            expand(i, i)
-            expand(i, i + 1)
+            expand(i,i)
+            expand(i,i+1)
 
         return count
+
+        

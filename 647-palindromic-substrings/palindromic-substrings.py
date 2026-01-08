d28815b38aea2cb5b5b15502ae4c65645e608f6e
sneha <30837089+itmesneha@users.noreply.github.com>
2026-01-08 15:18:39 +0530

    Time: 102 ms (95.61%) | Memory: 19.4 MB (23.35%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -1,21 +1,24 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
         '''
-        expand way?
+        expand around center
+        duplicates allowed so just keep count variable
         '''
 
         n = len(s)
         count = 0
+        
         def expand(left, right):
-            nonlocal count
+            count = 0
             while left >= 0 and right < n and s[left] == s[right]:
                 count += 1
                 left -= 1
                 right += 1
+            return count
 
         for i in range(n):
-            expand(i,i)
-            expand(i,i+1)
+            count += expand(i,i)
+            count += expand(i,i+1)
 
         return count
 

c433f38ba0ce130b617f34549fb8ecbdd7b8abc8
sneha <30837089+itmesneha@users.noreply.github.com>
2026-01-10 16:43:18 +0530

    Time: 3 ms (70.09%) | Memory: 19.6 MB (5.58%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,20 +1,35 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        n = len(s)
-        memo = [None for _ in range(n + 1)]
-        def fn(idx):
-            if idx >= n:
-                return True
-            if memo[idx] != None:
-                return memo[idx]
-            for length in range(1, n - idx + 1):
-                # print(s[idx : idx + length])
-                if s[idx : idx + length] in wordDict:
-                    if fn(idx + length): # don’t return immediately, check all
-                        memo[idx] = True
-                        return True
-                    # print('found')
-            memo[idx] = False
-            return False
+        '''
+        Word Break = DP on index; try all words at each cut.
+        If the first valid prefix leads to a dead end, you must backtrack and try another prefix.
+        '''
 
-        return fn(0)
+        memo = {}
+        def fn(s):
+            if s in memo:
+                return memo[s]
+            if s in wordDict:
+                memo[s] = True
+                return memo[s]
+            for i in range(len(s)+1):
+                if s[:i] in wordDict and fn(s[i:]):
+                    memo[s] = True
+                    return memo[s]
+
+            memo[s] = False
+            return memo[s]
+
+                # this is wrong:
+                # because if s = 'carscar' and wordDict = ['car', 'cars']
+                # then it will find 'car' first and the remaining 'scar' will fail
+                # and it will return false
+
+                # we also need it to fail and come back and find 'cars' first then 'car'
+                # then return true
+                # if s[:i] in wordDict:
+                #     return fn(s[i:])
+
+        if fn(s):
+            return True
+        return False
